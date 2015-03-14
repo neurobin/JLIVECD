6aae685524db753919b220d1b491bac11463ea34
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-03-14 23:29:53 +0600

    Bug Fixes

---
+++ install.sh
@@ -0,0 +1,13 @@
+cd $(dirname $0)
+sudo chmod -R 755 *
+sudo rm -rf /usr/local/JLIVECD
+sudo mkdir -p /usr/local/JLIVECD
+sudo cp -R . /usr/local/JLIVECD
+sudo chmod -R 755 /usr/local/JLIVECD
+sudo cp JLstart /bin
+sudo cp main/JLRefreshNetwork /bin
+sudo -s <<EOF
+echo -e "[Desktop Entry]\nName=JLIVECD\nType=Application\nExec=JLstart\nTerminal=true\nIcon=/usr/local/JLIVECD/main/48.png\nCategories=System;\nComment=Live CD/DVD customization tool (CLI)" > /usr/share/applications/JLIVECD.desktop
+EOF
+echo ".......Install complete!"
+echo ".......See the readme file provided with this software for instructions to use it......"
